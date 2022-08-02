51b7cad3c9133caded0880a9bd9682df259115b1
ervin <102836332+ErvinToepke@users.noreply.github.com>
2022-08-01 22:01:43 -0300

    nuevo

---
+++ practica0.py
@@ -0,0 +1,14 @@
+n1 = int(input("ingrese el primer numero "))
+n2 = int(input("ingrese el primer numero "))
+
+sum = n1+n2
+print("la suma es: ", sum)
+
+res = n1-n2
+print("la resta es: ", res)
+
+div = n1/n2
+print("la division es: ", div)
+
+mul = n1*n2
+print("la multiplicacion es: ", mul)
